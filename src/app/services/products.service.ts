5c85682f2143525e7aae00b9319033a1e8b8e334
Diego Bellantuono <diego.bellantuono@ict4b2b.com>
2020-05-11 13:18:41 +0200

    Primi css per il menu

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Product, Group } from '../models/item';
+import { Product, Group, Item } from '../models/item';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,14 @@ export class ProductsService {
 
   getAllGroups(): Group[] {
     const items : Group[] = Array(23).fill(0).map((x, i) => (
-      new Product(`Group ${i + 1}`,`Group ${i + 1}`,(i * 10))
+      new Product(`Group${i + 1}`,`Gr${i + 1}`,(i * 10))
+      ));
+    return items;
+  }
+
+  getAllMachines() : Item[] {
+    const items : Item[] = Array(23).fill(0).map((x, i) => (
+      new Product(`Code ${i + 1}`,`Machine ${i + 1}`,(i * 10))
       ));
     return items;
   }
