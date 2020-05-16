4b380c65ae5d383eae7145ab026f366a7831352f
Diego Bellantuono <diego.bellantuono@ict4b2b.com>
2020-05-16 17:01:17 +0200

    Disabilitati oData in SAP finché non saranno controllati

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   destination_ZSD_SP_SRV: "/destinations/ZSD_SP_SRV/",
   destination_LM_KAN: "/destinations/LM_KAN",
-  oData: true
+  oData: false
 };
