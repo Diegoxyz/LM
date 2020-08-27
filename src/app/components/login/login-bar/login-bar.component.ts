60492319f22f62d9963e2d163d134a9ef2e1d6ea
Diego Bellantuono <diego.bellantuono@ict4b2b.com>
2020-08-27 10:56:55 +0200

    Allineamento pulsanti e footer

--- src/app/components/login/login-bar/login-bar.component.ts
+++ src/app/components/login/login-bar/login-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-bar.component.css']
 })
 export class LoginBarComponent implements OnInit {
+
+  @Input() pageToShow = 1;
 
   constructor(private router: Router) { }
 
